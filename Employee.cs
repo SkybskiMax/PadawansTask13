726a83d332798cfc39ab1c960e6c7fc20b81e75d
Max <dranik15@mail.ru>
2019-02-20 18:57:01 +0300

    Create Employee.cs

---
+++ Employee.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PadawansTask13
+{
+    class Employee
+    {
+        private string Surname;
+        private int Age;
+
+        public Employee()
+        {
+
+        }
+
+        public Employee(string surname, int age)
+        {
+            Surname = surname;
+            Age = age;
+        }
+        
+        private string GetAgeString()
+        {
+            return Age.ToString();
+        }
+
+        public void SetNewSurname(string newSurname)
+        {
+            Surname = newSurname;
+        }
+
+        public string GetInfo()
+        {
+            return "Surname: " + Surname + ", Age: " + Age;
+        }
+    }
+}
